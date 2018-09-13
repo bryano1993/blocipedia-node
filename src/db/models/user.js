497e0872df1254e5d255e0238b534256864ef845
Bryan <bryano1993@gmail.com>
2018-09-12 19:10:49 -0700

    npm start and npm test error

---
+++ src/db/models/user.js
@@ -0,0 +1,28 @@
+"use strict";
+module.exports = (sequelize, DataTypes) => {
+  var User = sequelize.define(
+    "User",
+    {
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          isEmail: { msg: "must be a valid email" }
+        }
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false
+      }
+    },
+    {}
+  );
+  User.associate = function(models) {
+    // associations can be defined here
+  };
+  return User;
+};
