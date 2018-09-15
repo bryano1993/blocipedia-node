096a92c166295cb82c439614ef170a9b41e7877b
Bryan <bryano1993@gmail.com>
2018-09-14 19:17:07 -0700

    user-sign-in-and-out:complete

--- src/db/models/index.js
+++ src/db/models/index.js
@@ -1,18 +1,68 @@
+// "use strict";
+
+// const fs = require("fs");
+// const path = require("path");
+// const Sequelize = require("sequelize");
+// const basename = path.basename(__filename);
+// const env = process.env.NODE_ENV || "development";
+// const config = require(__dirname + "/../config/config.json")[env];
+// const db = {};
+
+// let sequelize;
+// if (config.use_env_variable) {
+//   sequelize = new Sequelize(process.env[config.use_env_variable], config);
+// } else {
+//   sequelize = new Sequelize(
+//     config.database,
+//     config.username,
+//     config.password,
+//     config
+//   );
+// }
+
+// fs.readdirSync(__dirname)
+//   .filter(file => {
+//     return (
+//       file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
+//     );
+//   })
+//   .forEach(file => {
+//     const model = sequelize["import"](path.join(__dirname, file));
+//     db[model.name] = model;
+//   });
+
+// Object.keys(db).forEach(modelName => {
+//   if (db[modelName].associate) {
+//     db[modelName].associate(db);
+//   }
+// });
+
+// db.sequelize = sequelize;
+// db.Sequelize = Sequelize;
+
+// module.exports = db;
+
 "use strict";
 
-const fs = require("fs");
-const path = require("path");
-const Sequelize = require("sequelize");
-const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || "development";
-const config = require(__dirname + "/../config/config.json")[env];
-const db = {};
+var fs = require("fs");
+var path = require("path");
+var Sequelize = require("sequelize");
+var basename = path.basename(__filename);
+var env = process.env.NODE_ENV || "development";
 
-let sequelize;
+const configPath = path.join(__dirname, "..", "config", "config.json");
+
+const configObj = require(configPath);
+
+const config = configObj["test"];
+
+// var config = require(__dirname + "/../config/config.json")[env];
+var db = {};
+
 if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+  var sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  sequelize = new Sequelize(
+  var sequelize = new Sequelize(
     config.database,
     config.username,
     config.password,
@@ -27,7 +77,7 @@ fs.readdirSync(__dirname)
     );
   })
   .forEach(file => {
-    const model = sequelize["import"](path.join(__dirname, file));
+    var model = sequelize["import"](path.join(__dirname, file));
     db[model.name] = model;
   });
 
