170d7a87aec24f337cac918c9a66b4d4d6eabf39
Bryan <bryano1993@gmail.com>
2018-09-18 12:00:51 -0700

    seeding-data:complete

---
+++ src/db/seeds/20180918061536-user.js
@@ -0,0 +1,42 @@
+"use strict";
+
+const faker = require("faker");
+
+let users = [];
+
+for (let i = 1; i <= 60; i++) {
+  users.push({
+    username: faker.internet.userName(),
+    email: faker.internet.email(),
+    password: faker.internet.password(),
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    role: "standard",
+    id: i
+  });
+}
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert("Users", users, {});
+
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+      Example:
+      return queryInterface.bulkInsert('Person', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete("Users", null, {});
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+      Example:
+      return queryInterface.bulkDelete('Person', null, {});
+    */
+  }
+};
